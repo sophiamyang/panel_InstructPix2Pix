509162ba7b4137006b86a3d18aa8fd75a11dc9a9
Sophia Yang <sophiayang@Sophias-Laptop.attlocal.net>
2023-01-27 15:10:28 -0600

    styling

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+import io
+
+import hvplot.pandas
+import numpy as np
+import panel as pn
+import param
 import PIL
 import requests
 import torch
-from diffusers import StableDiffusionInstructPix2PixPipeline
-import panel as pn
 
-pn.extension()
-import hvplot.pandas
-import io
-import param
-import numpy as np
+from diffusers import StableDiffusionInstructPix2PixPipeline
+
+pn.extension(template='bootstrap')
+pn.state.template.main_max_width = '690px'
+pn.state.template.accent_base_color = '#F08080'
+pn.state.template.header_background = '#F08080'
+
 
 # Model
 model_id = "timbrooks/instruct-pix2pix"
@@ -17,76 +23,83 @@ pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(
 ).to("cuda")
 
 
-def new_image(prompt, image, image_slider, guidance_slider, step):
+def new_image(prompt, image, img_guidance, guidance, steps):
     edit = pipe(
         prompt,
         image=image,
-        image_guidance_scale=image_slider,
-        guidance_scale=guidance_slider,
-        num_inference_steps=step,
+        image_guidance_scale=img_guidance,
+        guidance_scale=guidance,
+        num_inference_steps=steps,
     ).images[0]
     return edit
 
 
 # Panel widgets
-fi = pn.widgets.FileInput()
-inp = pn.widgets.TextInput(
+file_input = pn.widgets.FileInput(width=600)
+prompt = pn.widgets.TextInput(
     value="", placeholder="Enter image editing instruction here...", width=600
 )
-button_conversation = pn.widgets.Button(name="Run!", width=600)
-image_slider = pn.widgets.DiscreteSlider(
+img_guidance = pn.widgets.DiscreteSlider(
     name="Image guidance scale", options=list(np.arange(1, 10.5, 0.5)), value=1.5
 )
-guidance_slider = pn.widgets.DiscreteSlider(
+guidance = pn.widgets.DiscreteSlider(
     name="Guidance scale", options=list(np.arange(1, 10.5, 0.5)), value=7
 )
-step = pn.widgets.IntSlider(
-    name="Image guidance scale", start=1, end=100, step=1, value=20
+steps = pn.widgets.IntSlider(
+    name="Inference Steps", start=1, end=100, step=1, value=20
 )
+run_button = pn.widgets.Button(name="Run!", width=600)
+
 
 # define global variables to keep track of things
 convos = []  # store all panel objects in a list
 image = None
 filename = None
 
+def normalize_image(value, width):
+    """
+    normalize image to RBG channels and to the same size
+    """
+    b = io.BytesIO(value)
+    image = PIL.Image.open(b).convert("RGB")
+    aspect = image.size[1] / image.size[0]
+    height = int(aspect * width)
+    return image.resize((width, height), PIL.Image.ANTIALIAS)
 
-def get_conversations(_, img, image_slider, guidance_slider, step, width=600):
+def get_conversations(_, img, img_guidance, guidance, steps, width=600):
     """
     Get all the conversations in a Panel object
     """
     global image, filename
-    prompt = inp.value
-    inp.value = ""
+    prompt_text = prompt.value
+    prompt.value = ""
+    
     # if the filename changes, open the image again
-    if filename != fi.filename:
-        filename = fi.filename
-        b = io.BytesIO()
-        fi.save(b)
-        image = PIL.Image.open(b).convert("RGB")
-        # resize image if it's too large
-        aspect = image.size[1] / image.size[0]
-        height = int(aspect * width)
-        image = image.resize((width, height), PIL.Image.ANTIALIAS)
+    if filename != file_input.filename:
+        filename = file_input.filename
+        image = normalize_image(file_input.value, width)
         convos.clear()
-    if prompt:
+        
+    if prompt_text:
         # generate new image
-        image = new_image(prompt, image, image_slider, guidance_slider, step)
-        convos.append(pn.Row("\U0001F60A", pn.pane.Markdown(prompt, width=600)))
+        image = new_image(prompt_text, image, img_guidance, guidance, steps)
+        convos.append(pn.Row("\U0001F60A", pn.pane.Markdown(prompt_text, width=600)))
         convos.append(pn.Row("\U0001F916", image))
     return pn.Column(*convos)
 
 
 # bind widgets to functions
 interactive_conversation = pn.bind(
-    get_conversations, button_conversation, fi, image_slider, guidance_slider, step
+    get_conversations, run_button, file_input, img_guidance, guidance, steps
 )
-interactive_upload = pn.bind(pn.panel, fi, width=600)
+interactive_upload = pn.bind(pn.panel, file_input, width=600)
 
 # layout
 pn.Column(
-    pn.Column(fi, pn.panel(interactive_upload)),
+    pn.pane.Markdown("## \U0001F60A Upload an image file and start editing!"), 
+    pn.Column(file_input, pn.panel(interactive_upload)),
     pn.panel(interactive_conversation, loading_indicator=True),
-    inp,
-    pn.Row(button_conversation),
-    pn.Card(image_slider, guidance_slider, step, width=600, header="Advance settings"),
-).servable()
+    prompt,
+    pn.Row(run_button),
+    pn.Card(img_guidance, guidance, steps, width=600, header="Advance settings"),
+).servable(title='Stablel Diffusion InstructPix2pix Image Editing Chatbot')
